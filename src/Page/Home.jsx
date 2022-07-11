a10989f3f4eb8730a3284abd355121212b3727cf
n1k0lay <kolia.d97@gmail.com>
2022-07-11 21:02:02 +0500

    Ссылка с главной на график

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Home = () => {
     return (
         <div>
             Home
+            <hr/>
+            <NavLink to={'/graph'}>Страница с графиком</NavLink>
         </div>
     );
 };
